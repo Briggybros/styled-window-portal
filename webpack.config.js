e815e85e19b78dc1e3f7016bcc0b07dc16eec86e
Gregory Sims <briggybros.2@googlemail.com>
2018-01-28 17:07:53 +0000

    Updated build

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
     entry: path.join(__dirname, 'src', 'StyledWindowPortal.jsx'),
     output: {
-        path: path.join(__dirname, 'dist'),
+        path: path.join(__dirname, 'lib'),
         filename: 'styled-window-portal.min.js',
         libraryTarget: 'umd',
     },
@@ -22,13 +22,19 @@ module.exports = {
     },
     externals: {
         react: {
-            root: 'ReactDOM',
+            root: 'React',
             commonjs: 'react',
             commonjs2: 'react',
             amd: 'react',
         },
         'react-dom': {
-            root: 'React',
+            root: 'ReactDOM',
+            commonjs: 'react-dom',
+            commonjs2: 'react-dom',
+            amd: 'react-dom',
+        },
+        'external-styled-components': {
+            root: 'ReactDOM',
             commonjs: 'react-dom',
             commonjs2: 'react-dom',
             amd: 'react-dom',
