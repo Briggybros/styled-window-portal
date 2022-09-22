4f79f8a809793a365c9a3576d56f33ffee765d35
Gregory Sims <briggybros.2@googlemail.com>
2022-09-22 19:15:18 +0100

    Improve typing on window props

--- src/window-props.ts
+++ src/window-props.ts
@@ -1,13 +1,56 @@
+type WindowPropFactory<T> = (props: WindowProps, window: Window) => T;
+type WindowProp<T> = T | WindowPropFactory<T>;
+
 export type WindowProps = {
-  toolbar?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  location?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  directories?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  status?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  menubar?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  scrollbars?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  resizable?: Boolean | ((props: WindowProps, window: Window) => Boolean);
-  width?: Number | ((props: WindowProps, window: Window) => Number);
-  height?: Number | ((props: WindowProps, window: Window) => Number);
-  top?: Number | ((props: WindowProps, window: Window) => Number);
-  left?: Number | ((props: WindowProps, window: Window) => Number);
+  /**
+   * Whether or not to display the browser toolbar. IE and Firefox only.
+   */
+  toolbar?: WindowProp<boolean>;
+
+  /**
+   * Whether or not to display the address field. Opera only.
+   */
+  location?: WindowProp<boolean>;
+
+  /**
+   * 	Whether or not to add a status bar.
+   */
+  status?: WindowProp<boolean>;
+
+  /**
+   * Whether or not to display the menu bar.
+   */
+  menubar?: WindowProp<boolean>;
+
+  /**
+   * Whether or not to display scroll bars. IE, Firefox & Opera only.
+   */
+  scrollbars?: WindowProp<boolean>;
+
+  /**
+   * Whether or not the window is resizable. IE only.
+   */
+  resizable?: WindowProp<boolean>;
+
+  /**
+   * The width of the window. Min. value is 100.
+   */
+  width?: WindowProp<number>;
+
+  /**
+   * 	The height of the window. Min. value is 100.
+   */
+  height?: WindowProp<number>;
+
+  /**
+   * The top position of the window. Negative values not allowed.
+   */
+  top?: WindowProp<number>;
+
+  /**
+   * The left position of the window. Negative values not allowed.
+   */
+  left?: WindowProp<number>;
+
+  [key: string]: WindowProp<boolean | number | string> | undefined;
 };
